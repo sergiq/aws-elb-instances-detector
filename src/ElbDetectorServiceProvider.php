8dec0224b1509845214ad7640fd6a5ed7a4931ac
sergiq <licordelpolo@gmail.com>
2018-05-10 15:19:50 +0200

    comment

--- src/ElbDetectorServiceProvider.php
+++ src/ElbDetectorServiceProvider.php
@@ -11,6 +11,7 @@ class ElbDetectorServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // comment
         $this->mergeConfigFrom(__DIR__ . '/config/aws_elb_instance_detector.php', 'aws_elb_instance_detector');
     }
 }
