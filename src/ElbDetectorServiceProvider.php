d1fa876c2f33a808c949eda202dfeb4bf89f8f90
sergiq <licordelpolo@gmail.com>
2018-05-10 14:07:40 +0200

    added \\ to autoload

---
+++ src/ElbDetectorServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sergiqg\AwsElbInstancesDetector;
+
+use \Illuminate\Support\ServiceProvider;
+
+class ElbDetectorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register de Service Provider
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/config/aws_elb_instance_detector.php', 'aws_elb_instance_detector');
+    }
+}
